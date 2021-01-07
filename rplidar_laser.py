ecdbe86e51d9cae36b4dddbf4697adb74b212a2b
dfollereau <denis.follereau06@gmail.com>
2021-01-07 22:27:58 +0100

    adding mqtt functions and debug func

--- rplidar_laser.py
+++ rplidar_laser.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 Consume LIDAR measurement file and create an image for display.
  
@@ -13,28 +14,9 @@ from adafruit_rplidar import *
 import RPi.GPIO as GPIO
 import time
 from serial import SerialException
-
-# GPIO to LCD mapping
-LCD_RS = 7 # Pi pin 26
-LCD_E = 8 # Pi pin 24
-LCD_D4 = 25 # Pi pin 22
-LCD_D5 = 24 # Pi pin 18
-LCD_D6 = 23 # Pi pin 16
-LCD_D7 = 18 # Pi pin 12
-# Device constants
-LCD_CHR = True # Character mode
-LCD_CMD = False # Command mode
-LCD_CHARS = 16 # Characters per line (16 max)
-LCD_LINE_1 = 0x80 # LCD memory location for 1st line
-LCD_LINE_2 = 0xC0 # LCD memory location 2nd line
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM) # Use BCM GPIO numbers
-GPIO.setup(LCD_E, GPIO.OUT) # Set GPIO's to output mode
-GPIO.setup(LCD_RS, GPIO.OUT)
-GPIO.setup(LCD_D4, GPIO.OUT)
-GPIO.setup(LCD_D5, GPIO.OUT)
-GPIO.setup(LCD_D6, GPIO.OUT)
-GPIO.setup(LCD_D7, GPIO.OUT)
+import paho.mqtt.client as mqtt
+from config import *
+import logging_config as logging_config
 
 def lcd_init():
     lcd_write(0x33,LCD_CMD) # Initialize
@@ -85,7 +67,6 @@ def lcd_write(bits, mode):
     # Toggle 'Enable' pin
     lcd_toggle_enable()
 
-
 def lcd_text(message,line):
     # Send text to display
     message = message.ljust(LCD_CHARS," ")
@@ -98,39 +79,68 @@ def process_data(data, max_distance):
     right = 0
     back = 0
     left = 0
+    l = list()
 
     for angle in range(360):
         distance = data[angle]
         if angle==0:
             front = distance / 10
+            l.append(front)
         if angle==90:
             right = distance / 10
+            l.append(right)
         if angle==180:
             back = distance / 10
+            l.append(back)
         if angle==270:
             left = distance / 10
+            l.append(left)
 
-    print("**** RPLIDAR measure **** : [FRONT: "+str(front)+"cm] [RIGHT: "+str(right)+"cm] [BACK: "+str(back)+"cm] [LEFT: "+str(left)+"cm]")
+    # debug print("**** RPLIDAR measure **** : [FRONT: "+str(front)+"cm] [RIGHT: "+str(right)+"cm] [BACK: "+str(back)+"cm] [LEFT: "+str(left)+"cm]")
+    logging.debug("**** RPLIDAR measure **** : [FRONT: "+str(front)+"cm] [RIGHT: "+str(right)+"cm] [BACK: "+str(back)+"cm] [LEFT: "+str(left)+"cm]")
     lcd_text("F:"+str(int(front))+"cm R:"+str(int(right))+"cm", LCD_LINE_1)
     lcd_text("B:"+str(int(back))+"cm L:"+str(int(left))+"cm", LCD_LINE_2)
+    return l
+
+def GPIO_setup():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM) # Use BCM GPIO numbers
+    GPIO.setup(LCD_E, GPIO.OUT) # Set GPIO's to output mode
+    GPIO.setup(LCD_RS, GPIO.OUT)
+    GPIO.setup(LCD_D4, GPIO.OUT)
+    GPIO.setup(LCD_D5, GPIO.OUT)
+    GPIO.setup(LCD_D6, GPIO.OUT)
+    GPIO.setup(LCD_D7, GPIO.OUT)
 
 if __name__ == "__main__":
+    logging_config.debug_disable()
     # used to scale data to fit on the screen
     max_distance = 0
     scan_data = [0]*360
     # Initialize display
+    GPIO_setup()
     lcd_init()
     # Setup the RPLidar
     PORT_NAME = '/dev/ttyUSB0'
     lidar = RPLidar(None, PORT_NAME)
-    
+    client = mqtt.Client()
+    client.connect(RPI3_MQTT_BROKER_IP, RPI3_MQTT_BROKER_PORT, 60)
+
     try:
         print(lidar.info) # printed once! Laser device infos
 
         for scan in lidar.iter_scans():
             for (_, angle, distance) in scan:
                 scan_data[min([359, floor(angle)])] = distance
-            process_data(scan_data, max_distance)
+            measures = process_data(scan_data, max_distance)
+            length = len(measures)
+            i = 0
+            while i < length:
+                if(i == 0):
+                    answer =  str(measures[i])
+                else:
+                    answer = answer + "," + str(measures[i])
+            self.client.publish("distances", answer)
 
     except KeyboardInterrupt:
         print ("\nCtrl-C pressed from on_message()  Stopping GPIO and exiting properly...")
