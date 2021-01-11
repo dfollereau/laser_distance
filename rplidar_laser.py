d9f4a052d93eb98667804260d1d1221363dca3f2
dfollereau <denis.follereau06@gmail.com>
2021-01-11 08:54:28 +0100

    bug fixes and add mqtt

--- rplidar_laser.py
+++ rplidar_laser.py
@@ -9,6 +9,7 @@ Licensed under the MIT license.
 # RPLIDAR example: https://learn.adafruit.com/slamtec-rplidar-on-pi?view=all
 
 import os
+import sys
 from math import cos, sin, pi, floor
 from adafruit_rplidar import *
 import RPi.GPIO as GPIO
@@ -16,7 +17,8 @@ import time
 from serial import SerialException
 import paho.mqtt.client as mqtt
 from config import *
-import logging_config as logging_config
+import logging_config
+import logging
 
 def lcd_init():
     lcd_write(0x33,LCD_CMD) # Initialize
@@ -79,28 +81,28 @@ def process_data(data, max_distance):
     right = 0
     back = 0
     left = 0
-    l = list()
+    laser_measures = []
 
     for angle in range(360):
         distance = data[angle]
         if angle==0:
-            front = distance / 10
-            l.append(front)
+            front = int(distance / 10)
+            laser_measures.append(front)
         if angle==90:
-            right = distance / 10
-            l.append(right)
+            right = int(distance / 10)
+            laser_measures.append(right)
         if angle==180:
-            back = distance / 10
-            l.append(back)
+            back = int(distance / 10)
+            laser_measures.append(back)
         if angle==270:
-            left = distance / 10
-            l.append(left)
+            left = int(distance / 10)
+            laser_measures.append(left)
+        #print(str(angle) + " " + str(distance))
 
-    # debug print("**** RPLIDAR measure **** : [FRONT: "+str(front)+"cm] [RIGHT: "+str(right)+"cm] [BACK: "+str(back)+"cm] [LEFT: "+str(left)+"cm]")
-    logging.debug("**** RPLIDAR measure **** : [FRONT: "+str(front)+"cm] [RIGHT: "+str(right)+"cm] [BACK: "+str(back)+"cm] [LEFT: "+str(left)+"cm]")
-    lcd_text("F:"+str(int(front))+"cm R:"+str(int(right))+"cm", LCD_LINE_1)
-    lcd_text("B:"+str(int(back))+"cm L:"+str(int(left))+"cm", LCD_LINE_2)
-    return l
+    logging.debug("**** RPLIDAR measures **** : [FRONT: "+str(front)+"cm] [RIGHT: "+str(right)+"cm] [BACK: "+str(back)+"cm] [LEFT: "+str(left)+"cm]")
+    lcd_text("F:"+str(front)+"cm R:"+str(right)+"cm", LCD_LINE_1)
+    lcd_text("B:"+str(back)+"cm L:"+str(left)+"cm", LCD_LINE_2)
+    return laser_measures
 
 def GPIO_setup():
     GPIO.setwarnings(False)
@@ -113,6 +115,10 @@ def GPIO_setup():
     GPIO.setup(LCD_D7, GPIO.OUT)
 
 if __name__ == "__main__":
+
+    if(sys.version_info[0] < 3):
+        raise Exception("Must be using Python 3")
+
     logging_config.debug_disable()
     # used to scale data to fit on the screen
     max_distance = 0
@@ -132,28 +138,19 @@ if __name__ == "__main__":
         for scan in lidar.iter_scans():
             for (_, angle, distance) in scan:
                 scan_data[min([359, floor(angle)])] = distance
-            measures = process_data(scan_data, max_distance)
-            length = len(measures)
-            i = 0
-            while i < length:
-                if(i == 0):
-                    answer =  str(measures[i])
-                else:
-                    answer = answer + "," + str(measures[i])
-            self.client.publish("distances", answer)
+            laser = process_data(scan_data, max_distance)
+            mqtt_buf = ','.join(str(e) for e in laser)
+            #print(mqtt_buf)
+            client.publish("laser", mqtt_buf)
 
     except KeyboardInterrupt:
         print ("\nCtrl-C pressed from on_message()  Stopping GPIO and exiting properly...")
-    except (GMachineException) as e:
-        print('ERROR ' + str(e))
     except OSError as err:
         print("OS error: {0}".format(err))
     except FileNotFoundError:
         print("File does not exist")
-    except serial.serialutil.SerialException as e:
+    except SerialException as e:
         print("USB serial error")
-        print(str(e))
-    except adafruit_rplidar.RPLidarException as e:
         print(str(e))
     except ValueError:
         print("Could not convert data to an integer.")
